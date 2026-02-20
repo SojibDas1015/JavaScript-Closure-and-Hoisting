a9476306cb9cf66ebe4f80cb9eb263f097a578b4
Sojib Das <sojibdas1015@gmail.com>
2026-02-20 14:41:20 +0600

    javascript hoisting

---
+++ hoisting.js
@@ -0,0 +1,12 @@
+// internally javascript declare a variable top position in window
+// console.log(number)
+var number = 100
+console.log(number);
+
+function sum (num1, num2){
+    // internally javascript declare a variable top position in function
+    // console.log(total)
+    var total = num1 + num2;
+    console.log(total);
+}
+sum(10,10)
